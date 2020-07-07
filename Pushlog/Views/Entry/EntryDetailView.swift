d74434762a3d9d9f5e2e9dde6e646d151a20da6c
Laurens K <39766854+laurensk@users.noreply.github.com>
2020-07-07 14:19:20 +0200

    entry detail view

--- Pushlog/Views/Entry/EntryDetailView.swift
+++ Pushlog/Views/Entry/EntryDetailView.swift
@@ -13,15 +13,38 @@ struct EntryDetailView: View {
     let entry: Entry
     let log: Log
     
+    var color: Color
+    
+    init(entry: Entry, log: Log) {
+        self.entry = entry
+        self.log = log
+        
+        if entry.level.lowercased() == "error" {
+            self.color = Color(UIColor.systemRed)
+        } else if entry.level.lowercased() == "info" {
+            self.color = Color(UIColor.systemYellow)
+        } else {
+            self.color = Color(UIColor.systemGray)
+        }
+    }
+    
     var body: some View {
         VStack {
             ScrollView {
-                Text("\(log.displayName)")
                 HStack {
-                    Text("\(entry.level)")
-                    Text("\(DateUtils.getDateTimeString(DateUtils.timestampToDate(entry.timestamp)))")
-                }
-                Text("\(entry.value)")
+                    Text("\(log.displayName)").fontWeight(.semibold).font(.title).lineLimit(1)
+                    Spacer()
+                }.padding([.leading, .trailing]).padding(.top, 30)
+                
+                HStack {
+                    RoundedRectangle(cornerRadius: 4).frame(width: 100, height: 25).foregroundColor(color).overlay(Text("\(entry.level)").foregroundColor(Color.white).fontWeight(.bold)).padding(.trailing, 10)
+                    Text("\(DateUtils.getDateTimeString(DateUtils.timestampToDate(entry.timestamp)))").fontWeight(.semibold).foregroundColor(Color(UIColor.systemGray))
+                    Spacer()
+                }.padding([.leading, .trailing])
+                HStack {
+                    Text("\(entry.value)").multilineTextAlignment(.leading)
+                    Spacer()
+                }.padding().padding(.bottom, 20)
             }
         }.navigationBarTitle("Entry", displayMode: .inline)
     }
